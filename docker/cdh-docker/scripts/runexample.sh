7d5bf4e3b0d3e0ae0b8cc25275d0b3c82209cb23
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2017-10-19 18:14:41 -0500

    Manual perf
    Requires manually starting the perf command in a separate terminal...

--- docker/cdh-docker/scripts/runexample.sh
+++ docker/cdh-docker/scripts/runexample.sh
@@ -7,5 +7,22 @@ cat /etc/hostsbak > /etc/hosts
 rm -rf /etc/hostsbak
 #run wordcount example
 /usr/bin/restart_hadoop_spark.sh
-${HIBENCH_HOME}/bin/workloads/micro/wordcount/prepare/prepare.sh
-${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh
+echo "running wordcount"
+${HIBENCH_HOME}/bin/workloads/ml/als/prepare/prepare.sh
+echo 'prepare completed, attach perf, then press a key'
+read
+${HIBENCH_HOME}/bin/workloads/ml/als/spark/run.sh
+echo 'run completed, dettach perf, then press a key'
+read
+# echo "running sort"
+# ${HIBENCH_HOME}/bin/workloads/micro/sort/prepare/prepare.sh
+# perf stat -e LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches -o /tmp/sort.perf ${HIBENCH_HOME}/bin/workloads/micro/sort/hadoop/run.sh
+# echo "running dfsioe"
+# ${HIBENCH_HOME}/bin/workloads/micro/dfsioe/prepare/prepare.sh
+# perf stat -e LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches -o /tmp/dfsioe.perf ${HIBENCH_HOME}/bin/workloads/micro/dfsioe/hadoop/run.sh
+# echo "running terasort"
+# ${HIBENCH_HOME}/bin/workloads/micro/terasort/prepare/prepare.sh
+# perf stat -e LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches -o /tmp/terasort.perf ${HIBENCH_HOME}/bin/workloads/micro/terasort/hadoop/run.sh
+# echo "running sleep"
+# ${HIBENCH_HOME}/bin/workloads/micro/sleep/prepare/prepare.sh
+# perf stat -e LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches -o /tmp/sleep.perf ${HIBENCH_HOME}/bin/workloads/micro/sleep/hadoop/run.sh
--- docker/opensource-docker/scripts/runexample.sh
+++ docker/opensource-docker/scripts/runexample.sh
@@ -7,5 +7,18 @@ cat /etc/hostsbak > /etc/hosts
 rm -rf /etc/hostsbak
 #run wordcount example
 /usr/bin/restart_hadoop_spark.sh
+echo "running wordcount"
 ${HIBENCH_HOME}/bin/workloads/micro/wordcount/prepare/prepare.sh
 ${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh
+echo "running sort"
+${HIBENCH_HOME}/bin/workloads/micro/sort/prepare/prepare.sh
+${HIBENCH_HOME}/bin/workloads/micro/sort/hadoop/run.sh
+echo "running dfsioe"
+${HIBENCH_HOME}/bin/workloads/micro/dfsioe/prepare/prepare.sh
+${HIBENCH_HOME}/bin/workloads/micro/dfsioe/hadoop/run.sh
+echo "running terasort"
+${HIBENCH_HOME}/bin/workloads/micro/terasort/prepare/prepare.sh
+${HIBENCH_HOME}/bin/workloads/micro/terasort/hadoop/run.sh
+echo "running sleep"
+${HIBENCH_HOME}/bin/workloads/micro/sleep/prepare/prepare.sh
+${HIBENCH_HOME}/bin/workloads/micro/sleep/hadoop/run.sh
